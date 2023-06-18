2184f858227d2241e94f395071d19ef8595597ed
BeYoungWu <byungwoo97@naver.com>
2023-06-18 11:39:56 +0900

    day0

---
+++ Test/src/level0/q1.java
@@ -0,0 +1,14 @@
+package level0;
+
+public class q1 {
+
+	public static void main(String[] args) {
+		
+		int n = 123;
+		String answer = String.valueOf(n);
+        
+		System.out.println(answer);
+
+	}
+
+}
---
+++ Test/src/level0/q2.java
@@ -0,0 +1,9 @@
+package level0;
+
+public class q2 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
