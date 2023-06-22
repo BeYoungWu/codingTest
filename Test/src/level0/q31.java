256225e605b57d6da6e006685643db21804d7fc9
BeYoungWu <byungwoo97@naver.com>
2023-06-22 16:08:02 +0900

    day4

---
+++ Test/src/level0/q31.java
@@ -0,0 +1,18 @@
+package level0;
+
+import java.util.Arrays;
+
+public class q31 {
+
+	public static void main(String[] args) {
+		// 배열 자르기
+		
+		int[] numbers = {1, 3, 5};
+		int num1 = 1;
+		int num2 = 2;
+		
+		int[] answer = Arrays.copyOfRange(numbers, num1, num2+1);
+
+	}
+
+}
