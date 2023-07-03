7e386e1447e9f32e6c00ff34b0ffe6d5929e0af4
BeYoungWu <byungwoo97@naver.com>
2023-07-03 21:47:16 +0900

    day15

---
+++ Test/src/level2/q6.java
@@ -0,0 +1,29 @@
+package level2;
+
+public class q6 {
+
+	public static void main(String[] args) {
+		// 숫자의 표현
+		
+		int n = 15;
+		
+		int answer = 0;
+		
+		for (int i=1;i<=n;i++) {
+			int sum = 0;
+			for (int j=i;j<=n;j++) {
+				sum += j;
+				if(sum == n) {
+					answer++;
+					break;
+				} else if (sum>n) {
+					break;
+				}
+			}
+		}
+		
+		// 다른 답안 : 정수론 (주어진 수의 홀수 약수 개수와 같다)
+
+	}
+
+}
