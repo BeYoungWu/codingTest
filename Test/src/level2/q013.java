f304d1f1284f5ca8491b986eb30f98003ad17c31
BeYoungWu <byungwoo97@naver.com>
2023-07-08 17:07:15 +0900

    day20

--- Test/src/level2/q013.java
+++ Test/src/level2/q013.java
@@ -1,5 +1,7 @@
 package level2;
 
+import java.util.Arrays;
+
 public class q013 {
 
 	public static void main(String[] args) {
@@ -9,9 +11,28 @@ public class q013 {
 		int limit = 100;
 		
 		int answer = 0;
-		
-		
 
+		Arrays.sort(people);
+		int index = 0;
+		for (int i=people.length-1;i >= index;i--) {
+			if (people[i] + people[index] <= limit) {
+				index++;
+				answer++;
+			} else {
+				answer ++;
+			}
+		}
+
+		// 다른 답안
+		Arrays.sort(people);
+		int i = 0, j = people.length - 1;
+		for (;i<j;--j) {
+			if (people[i] + people[j] <= limit) {
+				++i;
+			}
+		}
+		answer = people.length - i;
+		
 	}
 
 }
