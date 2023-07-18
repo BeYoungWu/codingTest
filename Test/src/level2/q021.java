eb15e424364f9ebc80c5474329498e59b4aa55d3
BeYoungWu <byungwoo97@naver.com>
2023-07-18 23:52:30 +0900

    day30

--- Test/src/level2/q021.java
+++ Test/src/level2/q021.java
@@ -3,9 +3,23 @@ package level2;
 public class q021 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// n^2 배열 자르기
 		
-
+		int n = 3;
+		long left = 2;
+		long right = 5;
+		
+		int len = (int)(right - left);
+		int[] answer = new int[len+1];
+		
+        int idx = 0;
+		for (long i=left;i<=right;i++) {
+			long a = i/n;
+			long b = i%n;
+			answer[idx] = Math.max((int)a, (int)b) + 1;
+            idx++;
+		}
+		
 	}
 
 }
