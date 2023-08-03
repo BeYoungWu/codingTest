995bde6556ea7833589f5fb43b1e517d12984326
BeYoungWu <byungwoo97@naver.com>
2023-08-03 15:44:15 +0900

    day42

--- Test/src/level2/softeer01.java
+++ Test/src/level2/softeer01.java
@@ -1,33 +1,69 @@
 package level2;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.StringTokenizer;
+
+class Jewelry {
+	private int weight;
+	private int price;
+	
+	public Jewelry(int weight, int price) {
+		this.weight = weight;
+		this.price = price;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public int getPrice() {
+		return price;
+	}
+}
 
 public class softeer01 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// 금고털이
-		
-		Scanner sc = new Scanner(System.in);
-		int W = sc.nextInt();
-		int N = sc.nextInt();
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int W,rest = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 		int sum = 0;
-		Set<Integer> set = new HashSet<>();
+		ArrayList<Jewelry> list = new ArrayList<>();
 		for (int i=0;i<N;i++) {
-			int weight = sc.nextInt();
-			int num = sc.nextInt();
-			for (int j=0;j<num;j++) {
-				sum += weight;
-				if (sum>W) {
-					sum -= weight;
-				}
+			st = new StringTokenizer(in.readLine());
+			int weight = Integer.parseInt(st.nextToken());
+			int price = Integer.parseInt(st.nextToken());
+			Jewelry j = new Jewelry(weight, price);
+			list.add(j);
+		}
+		list.sort((s1, s2) -> Integer.compare(s2.getPrice(), s1.getPrice()));
+		int idx = 0;
+		while(true) {
+			int p = list.get(idx).getPrice();
+			int w = list.get(idx).getWeight();
+			if (rest-w > 0) {
+				sum += w*p;
+				rest -= w;
+			}else {
+				sum += rest*p;
+				rest -= rest;
 			}
+			if (rest==0 || idx==N) {
+				break;
+			}
+			idx++;
 		}
 		System.out.println(sum);
 		
-		
-
 	}
-
+	
 }
