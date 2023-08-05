e42e2ffed595de4dac424bf300eab404e1958898
BeYoungWu <byungwoo97@naver.com>
2023-08-05 15:07:45 +0900

    day45

---
+++ Test/src/test/test0303.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class test0303 {
+
+	static Queue<Point> qpoint = new LinkedList<>();
+	
+	static String[][] arr1;
+	static String[][] arr2;
+	static String[][] arr3;
+	static int[][] chkArr1;
+	static int[][] chkArr2;
+	static int[][] chkArr3;
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
