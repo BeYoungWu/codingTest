5d3aa1c5770148b0db4fa46f19773e1c0f118cca
BeYoungWu <byungwoo97@naver.com>
2023-08-27 23:05:13 +0900

    day60

--- Test/src/level3/q004.java
+++ Test/src/level3/q004.java
@@ -9,9 +9,28 @@ public class q004 {
 		int[][] computers = {{1, 1, 0},{1, 1, 0},{0, 0, 1}};
 		
 		int answer = 0;
-		
-		
 
+		boolean[] chk = new boolean[n];
+		
+		for (int i=0;i<n;i++) {
+			if (!chk[i]) {
+				dfs(computers, i, chk);
+				answer++;
+			}
+		}
+		
+		System.out.println(answer);
+	}
+	
+	public static boolean[] dfs(int[][] computers, int i, boolean[] chk) {
+		chk[i] = true;
+		
+		for (int j=0;j<computers.length;j++) {
+			if (i!=j && computers[i][j]==1 && !chk[j]) {
+				chk = dfs(computers, j, chk);
+			}
+		}
+		return chk;
 	}
 
 }
