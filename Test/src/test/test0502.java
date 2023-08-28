15c906842cdf2966e09a22c5773e89f22015dbe3
BeYoungWu <byungwoo97@naver.com>
2023-08-28 23:54:43 +0900

    day61

--- Test/src/test/test0502.java
+++ Test/src/test/test0502.java
@@ -2,12 +2,22 @@ package test;
 
 public class test0502 {
 
+	static boolean[][] visited;
+	
 	public static void main(String[] args) {
 		String[] maps = { "--a-", "--d-", "b-c-", "---c" };
 		String word = "abc";
 		
+		visited = new boolean[maps.length][maps[0].length()];
+		
+		dfs(maps, visited);
+		
+	}
+	
+	public static boolean[][] dfs(String[] maps, boolean[][] visited) {
 		
 		
+		return visited;
 	}
 
 }
