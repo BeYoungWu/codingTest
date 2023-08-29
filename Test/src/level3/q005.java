4bda637621a0d3879a67112605bc2b93abd3d9fa
BeYoungWu <byungwoo97@naver.com>
2023-08-29 23:32:55 +0900

    day62

---
+++ Test/src/level3/q005.java
@@ -0,0 +1,18 @@
+package level3;
+
+public class q005 {
+
+	public static void main(String[] args) {
+		// 단어 변환
+		
+		String begin = "hit";
+		String target = "cog";
+		String[] words = {"hot", "dot", "dog", "lot", "log", "cog"};
+
+		int answer = 0;
+		
+		
+		
+	}
+
+}
