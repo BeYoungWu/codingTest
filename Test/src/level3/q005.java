02a5f718cf40bf17cb3ca9f2196f23c72b0f0481
BeYoungWu <byungwoo97@naver.com>
2023-08-31 16:39:55 +0900

    day63

--- Test/src/level3/q005.java
+++ Test/src/level3/q005.java
@@ -2,6 +2,9 @@ package level3;
 
 public class q005 {
 
+	static boolean[] visited;
+	static int answer = 0;
+	
 	public static void main(String[] args) {
 		// 단어 변환
 		
@@ -9,10 +12,18 @@ public class q005 {
 		String target = "cog";
 		String[] words = {"hot", "dot", "dog", "lot", "log", "cog"};
 
-		int answer = 0;
+		visited = new boolean[words.length];
+		dfs(begin, target, words, 0);
 		
+		System.out.println(answer);
 		
-		
+	}
+	
+	public static void dfs(String begin, String target, String[] words, int cnt) {
+		if (begin.equals(target)) {
+			answer = cnt;
+			return;
+		}
 	}
 
 }
