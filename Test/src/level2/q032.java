0b96211cbe0d384e6226f745de63b093b3b4e0f5
BeYoungWu <byungwoo97@naver.com>
2023-10-19 15:51:56 +0900

    day68

---
+++ Test/src/level2/q032.java
@@ -0,0 +1,22 @@
+package level2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class q032 {
+
+	public static void main(String[] args) {
+		// 다리를 지나는 트럭
+		
+		int bridge_length = 2;
+		int weight = 10;
+		int[] truck_weights = {7, 4, 5, 6};
+		
+		int answer = 0;
+		
+		Queue<Integer> bridge = new LinkedList<>();
+		
+		System.out.println(answer);
+	}
+
+}
---
+++ Test/src/test/test0801.java
@@ -0,0 +1,11 @@
+package test;
+
+public class test0801 {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
