26cd3fa5eb86cf48e9b2466ffdd1cf5dbfd8aa3d
BeYoungWu <byungwoo97@naver.com>
2023-10-25 22:53:09 +0900

    day70

--- Test/src/level2/q032.java
+++ Test/src/level2/q032.java
@@ -22,20 +22,20 @@ public class q032 {
 			int truck = truck_weights[i];
 			
 			while (true) {
-				if(bridge.isEmpty()) {// 큐에 아무것도 없는 경우 = 어떠한 트럭도 다리위에 없음
+				if(bridge.isEmpty()) {// 다리 위에 트럭이 없는 경우
 					bridge.add(truck);
 					sum += truck;
 					time++;
 					break;
-				} else if (bridge.size()==bridge_length) {// 큐에 다리 길이만큼 트럭이 다 찬 경우
+				} else if (bridge.size()==bridge_length) {// 다리 위에 트럭이 가득 찬 경우
 					sum -= bridge.poll();
-				} else {// 다리 길이만큼 큐가 차지않았음
-					if (sum+truck <= weight) {// weight 값을 넘지 않는 선에서 새로운 트럭을 다리에 올려줌
+				} else {// 다리 위에 트럭이 가득 차지 않고 있는 경우
+					if (sum+truck <= weight) {// weight 값을 넘지 않으면서 새로운 트럭을 다리에 올리기
 						bridge.add(truck);
 						sum += truck;
 						time++;
 						break;
-					} else {// 넘는다면 0을 넣어 이미 큐에 있는 트럭이 다리를 건너게 만듬
+					} else {// weight 값을 넘는다면 0을 넣어 이미 다리 위에 있는 트럭이 전진
 						bridge.add(0);
 						time++;
 					}
